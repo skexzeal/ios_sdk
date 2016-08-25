16b4a1c890245a5cf6fdd6aecffe9fcff9f7eaec
Pedro <pedro@adeven.com>
2016-08-25 16:36:19 +0200

    New version v4.9.0

--- AdjustTests/ADJPackageFields.m
+++ AdjustTests/ADJPackageFields.m
@@ -16,7 +16,7 @@
 
     // default values
     self.appToken = @"qwerty123456";
-    self.clientSdk = @"ios4.8.4";
+    self.clientSdk = @"ios4.9.0";
     self.suffix = @"";
     self.environment = @"sandbox";
 
